361ef372208f75b12460b1618f797713f51ea90e
joemarlo <joe.marlo.09@gmail.com>
2020-02-27 15:10:02 -0500

    added work status

--- ATUS.R
+++ ATUS.R
@@ -35,9 +35,10 @@ age.weighted <- atussum_2018 %>%
                values_to = 'time') %>% 
   mutate(weighted.minutes = TUFINLWGT * time) %>% 
   group_by(activity, TEAGE) %>% 
-  summarize(weighted.minutes = sum(weighted.minutes) / sum(TUFINLWGT))
+  summarize(weighted.minutes = sum(weighted.minutes) / sum(TUFINLWGT)) %>% 
+  ungroup()
   
-# area plot of watching tv by age
+# scatter plot of watching tv by age
 age.weighted %>% 
   filter(activity %in% c('t120303', 't120304')) %>% #watching TV
   group_by(TEAGE) %>% 
@@ -61,21 +62,22 @@ age.weighted <- age.weighted %>%
 
 # facet plots of all the activities by age
 age.weighted %>% 
+  filter(Code != '50') %>% 
   group_by(TEAGE, Description) %>% 
   summarize(weighted.minutes = sum(weighted.minutes),
             weighted.hours = sum(weighted.minutes)/60) %>% 
   ggplot(aes(x = TEAGE, y = weighted.hours)) +
-  geom_smooth(method = lm, formula = y ~ splines::bs(x, 3),
+  geom_smooth(method = lm, formula = y ~ splines::bs(x, 4),
               se = FALSE, 
               color = blog.color,
               linetype = 'dashed') +
   geom_point(alpha = 0.2, color = blog.color) +
   facet_wrap(~Description, scales = 'free_y', ncol = 3) +
-  labs(title = "Average time watching television by age",
+  labs(title = "Average daily time spent on activity by age",
        subtitle = "2018 American Time Use Survey",
        x = "Age",
        y = 'Average hours per day') +
-  theme(strip.text = element_text(size = 7))
+  theme(strip.text = element_text(size = 6))
   
 ggsave(filename = "Plots/Activities_by_age.svg",
        plot = last_plot(),
@@ -83,3 +85,61 @@ ggsave(filename = "Plots/Activities_by_age.svg",
        width = 9,
        height = 9)
   
+
+# split by working status -------------------------------------------------
+
+# calculate the weights and join with the simple codes
+work.age <- atussum_2018 %>% 
+  select(TUCASEID,
+         TUFINLWGT,
+         TEAGE,
+         matches('^t[0-9]')) %>% 
+  pivot_longer(cols = -c('TUCASEID', 'TUFINLWGT', 'TEAGE'),
+               names_to = "activity",
+               values_to = 'time') %>% 
+  mutate(weighted.minutes = TUFINLWGT * time) %>% 
+  mutate(Code = str_extract(activity, '^*[0-9][0-9]')) %>% 
+  left_join(simple_codes)
+
+# find the IDs where people we're working (more than 120min)
+#  then join  back to work.age then summarize the activities
+#  by age and this new work status
+work.age.weighted <- work.age %>% 
+  filter(Code == '05') %>% 
+  group_by(TUCASEID) %>% 
+  mutate(work.status = sum(weighted.minutes) > 120) %>%
+  select(TUCASEID, work.status) %>%
+  distinct() %>% 
+  right_join(work.age) %>% 
+  group_by(Code, Description, TEAGE, work.status) %>% 
+  summarize(weighted.minutes = sum(weighted.minutes) / sum(TUFINLWGT)) %>% 
+  ungroup()
+
+# facet plots of all the activities by age split by working status
+work.age.weighted %>% 
+  filter(Code != '50',
+         Code != '05') %>% 
+  group_by(TEAGE, Description, work.status) %>% 
+  summarize(weighted.minutes = sum(weighted.minutes),
+            weighted.hours = sum(weighted.minutes)/60) %>% 
+  ggplot(aes(x = TEAGE, y = weighted.hours, group = work.status, color = work.status)) +
+  geom_smooth(method = lm, formula = y ~ splines::bs(x, 4),
+              se = FALSE, 
+              linetype = 'dashed') +
+  geom_point(alpha = 0.2) +
+  scale_color_manual(values = c('#c2886d', blog.color), label = c('Work day', 'Not a work day')) +
+  facet_wrap(~Description, scales = 'free_y', ncol = 3) +
+  labs(title = "Average daily time spent on activity by age and working status",
+       subtitle = "2018 American Time Use Survey",
+       x = "Age",
+       y = 'Average hours per day') +
+  theme(strip.text = element_text(size = 6),
+        legend.position = 'bottom',
+        legend.title = element_blank())
+
+ggsave(filename = "Plots/Activities_by_age_work.svg",
+       plot = last_plot(),
+       device = "svg",
+       width = 9,
+       height = 9)
+
--- README.md
+++ README.md
@@ -4,11 +4,19 @@ Analysis of American Time Use Survey
 
 ## To-do list
 - [x] Find the right data
+- [X] Consoldiated the features
 - [x] Summarize of activities by age
-- [ ] Summarize of activities by age and working status
+- [x] Summarize of activities by age and working status
+- [ ] Double check specific activities are categorized correctly (e.g. travel should not include commuting)
 - [ ] Explore trends by income
 
+<br>
 
 <p align="center">
 <img src="Plots/Activities_by_age.svg" width=79%>
 </p>
+
+<p align="center">
+<img src="Plots/Activities_by_age_work.svg" width=79%>
+</p>
+
