6d77fa81a92c024f1527121316d8c789bd0c0247
joemarlo <joe.marlo.09@gmail.com>
2020-02-28 14:17:45 -0500

    deleted heritage code; reran plots with new weighting mechanism

--- ATUS.R
+++ ATUS.R
@@ -68,8 +68,41 @@ weigh_it(df = atussum_2018, groups = c('TEAGE', 'TESEX'), activities = c('t12030
   theme(legend.title = element_blank(),
         legend.position = 'bottom')
 
+ggsave(filename = "Plots/TV_by_age_sex.svg",
+       plot = last_plot(),
+       device = "svg",
+       width = 9,
+       height = 5)
 
 # adding simple codes -----------------------------------------------------
+
+# facet plots of all the activities by age
+weigh_it(df = atussum_2018, groups = c('TEAGE')) %>% 
+  fuzzyjoin::regex_left_join(x = ., y = curated.codes, by = c(activity = 'activity')) %>%
+  filter(activity.y != 't50.*') %>% 
+  group_by(TEAGE, description) %>% 
+  summarize(weighted.minutes = sum(weighted.minutes)) %>%
+  ggplot(aes(x = TEAGE, y = weighted.minutes)) +
+  geom_point(alpha = 0.2, color = blog.color) +
+  geom_smooth(method = lm, formula = y ~ splines::bs(x, 4),
+              se = FALSE, 
+              color = blog.color,
+              linetype = 'dashed') +
+  scale_y_continuous(label = function(x) sprintf("%2d:%02d", as.integer(x %/% 60), as.integer(x %% 60))) +
+  facet_wrap(~description, scales = 'free_y', ncol = 3) +
+  labs(title = "Average daily time spent on activity by age",
+       subtitle = "2018 American Time Use Survey",
+       x = "Age",
+       y = 'Average hours:minutes per day') +
+  theme(strip.text = element_text(size = 6)) +
+  theme(legend.title = element_blank(),
+        legend.position = 'bottom')
+
+ggsave(filename = "Plots/Activities_by_age.svg",
+       plot = last_plot(),
+       device = "svg",
+       width = 9,
+       height = 11)
 
 # facet plots of all the activities by age and gender
 weigh_it(df = atussum_2018, groups = c('TEAGE', 'TESEX')) %>% 
@@ -85,14 +118,20 @@ weigh_it(df = atussum_2018, groups = c('TEAGE', 'TESEX')) %>%
               # color = blog.color,
               linetype = 'dashed') +
   scale_y_continuous(label = function(x) sprintf("%2d:%02d", as.integer(x %/% 60), as.integer(x %% 60))) +
-  facet_wrap(~description, scales = 'free_y', ncol = 4) +
-  labs(title = "Average daily time spent on activity by age",
+  facet_wrap(~description, scales = 'free_y', ncol = 3) +
+  labs(title = "Average daily time spent on activity by age and sex",
        subtitle = "2018 American Time Use Survey",
        x = "Age",
        y = 'Average hours:minutes per day') +
   theme(strip.text = element_text(size = 6)) +
   theme(legend.title = element_blank(),
         legend.position = 'bottom')
+
+ggsave(filename = "Plots/Activities_by_age_sex.svg",
+       plot = last_plot(),
+       device = "svg",
+       width = 9,
+       height = 11)
 
 
 # add indicator for work day ----------------------------------------------
@@ -106,109 +145,14 @@ sum.2018 <- atussum_2018 %>%
   select(work.status) %>% 
   bind_cols(atussum_2018)
 
-# facet plots of all the activities by age and gender
-weigh_it(df = sum.2018, groups = c('TEAGE', 'TESEX', 'work.status')) %>% 
+# facet plots of all the activities by age
+weigh_it(df = sum.2018, groups = c('TEAGE', 'work.status')) %>% 
   # match based on regex code in curated.codes
   fuzzyjoin::regex_left_join(x = ., y = curated.codes, by = c(activity = 'activity')) %>%
   # remove data code
   filter(activity.y != 't50.*') %>% 
-  mutate(TESEX = recode(as.character(TESEX), '1' = 'Male', '2' = 'Female'),
-         work.status = recode(as.character(work.status), 'TRUE' = 'Work day', 'FALSE' = 'Not a work day')) %>% 
-  mutate(Group = paste0(work.status, ' - ', TESEX)) %>% 
-  group_by(TEAGE, Group, description) %>%
-  summarize(weighted.minutes = sum(weighted.minutes)) %>% 
-  ggplot(aes(x = TEAGE, y = weighted.minutes, group = Group, color = Group)) +
-  geom_point(alpha = 0.1) +
-  geom_smooth(method = lm, formula = y ~ splines::bs(x, 4),
-              se = FALSE, 
-              linetype = 'dashed') +
-  scale_y_continuous(label = function(x) sprintf("%2d:%02d", as.integer(x %/% 60), as.integer(x %% 60))) +
-  scale_color_manual(values = c('darkolivegreen4', 'darkolivegreen2','coral4', 'coral2')) +
-  facet_wrap(~description, scales = 'free_y', ncol = 3) +
-  labs(title = "Average daily time spent on activity by age and working status",
-       subtitle = "2018 American Time Use Survey",
-       caption = 'Work day defined as working two or more hours',
-       x = "Age",
-       y = 'Average hours:minutes per day') +
-  theme(strip.text = element_text(size = 6),
-        legend.position = 'bottom',
-        legend.title = element_blank(),
-        plot.caption = element_text(face = "italic",
-                                    size = 5,
-                                    color = 'grey50'))
-
-
-# EDA 2 -------------------------------------------------------------------
-
-# add the field description
-age.weighted <- age.weighted %>% 
-  mutate(Code = str_extract(activity, '^*[0-9][0-9]')) %>% 
-  left_join(simple.codes)
-
-# facet plots of all the activities by age
-age.weighted %>% 
-  filter(Code != '50') %>% 
-  group_by(TEAGE, Description) %>% 
-  summarize(weighted.minutes = sum(weighted.minutes)) %>%
-  ggplot(aes(x = TEAGE, y = weighted.minutes)) +
-  geom_point(alpha = 0.2, color = blog.color) +
-  geom_smooth(method = lm, formula = y ~ splines::bs(x, 4),
-              se = FALSE, 
-              color = blog.color,
-              linetype = 'dashed') +
-  scale_y_continuous(label = function(x) sprintf("%2d:%02d", as.integer(x %/% 60), as.integer(x %% 60))) +
-  facet_wrap(~Description, scales = 'free_y', ncol = 3) +
-  labs(title = "Average daily time spent on activity by age",
-       subtitle = "2018 American Time Use Survey",
-       x = "Age",
-       y = 'Average hours:minutes per day') +
-  theme(strip.text = element_text(size = 6))
-
-ggsave(filename = "Plots/Activities_by_age.svg",
-       plot = last_plot(),
-       device = "svg",
-       width = 9,
-       height = 11)
-
-
-# split by working status -------------------------------------------------
-
-# calculate the weights and join with the simple codes
-work.age <- atussum_2018 %>% 
-  select(TUCASEID,
-         TUFINLWGT,
-         TEAGE,
-         matches('^t[0-9]')) %>% 
-  pivot_longer(cols = -c('TUCASEID', 'TUFINLWGT', 'TEAGE'),
-               names_to = "activity",
-               values_to = 'time') %>% 
-  mutate(Code = str_extract(activity, '^*[0-9][0-9]')) %>%
-  left_join(simple.codes)
-
-# find the IDs where people we're working (more than 120min)
-#  then join  back to work.age then summarize the activities
-#  by age and this new work status
-work.age.weighted <- work.age %>% 
-  filter(Code == '05') %>% # exclude generic data code
-  group_by(TUCASEID) %>% 
-  mutate(work.status = sum(time) > 120) %>%
-  select(TUCASEID, work.status) %>%
-  distinct() %>% 
-  right_join(work.age) %>% 
-  mutate(weighted.minutes = TUFINLWGT * time) %>% 
-  group_by(activity, TEAGE, work.status) %>% 
-  summarize(weighted.minutes = sum(weighted.minutes) / sum(TUFINLWGT)) %>% 
-  ungroup()
-
-# add the field description
-work.age.weighted <- work.age.weighted %>% 
-  mutate(Code = str_extract(activity, '^*[0-9][0-9]')) %>% 
-  left_join(simple.codes)
-
-# facet plots of all the activities by age split by working status
-work.age.weighted %>% 
-  filter(Code != '50') %>% # exclude generic data code
-  group_by(TEAGE, Description, work.status) %>%
+  mutate(work.status = recode(as.character(work.status), 'TRUE' = 'Work day', 'FALSE' = 'Leisure day')) %>% 
+  group_by(TEAGE, work.status, description) %>%
   summarize(weighted.minutes = sum(weighted.minutes)) %>% 
   ggplot(aes(x = TEAGE, y = weighted.minutes, group = work.status, color = work.status)) +
   geom_point(alpha = 0.1) +
@@ -216,8 +160,8 @@ work.age.weighted %>%
               se = FALSE, 
               linetype = 'dashed') +
   scale_y_continuous(label = function(x) sprintf("%2d:%02d", as.integer(x %/% 60), as.integer(x %% 60))) +
-  scale_color_manual(values = c(blog.color, '#c2886d'), label = c('Leisure day', 'Work day')) +
-  facet_wrap(~Description, scales = 'free_y', ncol = 3) +
+  scale_color_manual(values = c(blog.color, 'coral3')) +
+  facet_wrap(~description, scales = 'free_y', ncol = 3) +
   labs(title = "Average daily time spent on activity by age and working status",
        subtitle = "2018 American Time Use Survey",
        caption = 'Work day defined as working two or more hours',
@@ -237,20 +181,62 @@ ggsave(filename = "Plots/Activities_by_age_work.svg",
        height = 11)
 
 
+# facet plots of all the activities by age and gender for work only days
+weigh_it(df = sum.2018, groups = c('TEAGE', 'TESEX', 'work.status')) %>% 
+  # match based on regex code in curated.codes
+  fuzzyjoin::regex_left_join(x = ., y = curated.codes, by = c(activity = 'activity')) %>%
+  # remove data code
+  filter(activity.y != 't50.*',
+         work.status == TRUE) %>% 
+  mutate(TESEX = recode(as.character(TESEX), '1' = 'Male', '2' = 'Female')) %>% 
+  group_by(TEAGE, TESEX, description) %>%
+  summarize(weighted.minutes = sum(weighted.minutes)) %>% 
+  ggplot(aes(x = TEAGE, y = weighted.minutes, group = TESEX, color = TESEX)) +
+  geom_point(alpha = 0.1) +
+  geom_smooth(method = lm, formula = y ~ splines::bs(x, 4),
+              se = FALSE, 
+              linetype = 'dashed') +
+  scale_y_continuous(label = function(x) sprintf("%2d:%02d", as.integer(x %/% 60), as.integer(x %% 60))) +
+  # scale_color_manual(values = c('darkolivegreen4', 'darkolivegreen2','coral4', 'coral2')) +
+  facet_wrap(~description, scales = 'free_y', ncol = 3) +
+  labs(title = "Average daily time spent on activity by age and gender during work days",
+       subtitle = "2018 American Time Use Survey",
+       caption = 'Work day defined as working two or more hours',
+       x = "Age",
+       y = 'Average hours:minutes per day') +
+  theme(strip.text = element_text(size = 6),
+        legend.position = 'bottom',
+        legend.title = element_blank(),
+        plot.caption = element_text(face = "italic",
+                                    size = 5,
+                                    color = 'grey50'))
+
+ggsave(filename = "Plots/Activities_by_age_sex_workonly.svg",
+       plot = last_plot(),
+       device = "svg",
+       width = 9,
+       height = 11)
+
+
+
+# gif ---------------------------------------------------------------------
+
 # facet plots of all the activities by age split by working status
-work.gif <- work.age.weighted %>% 
-  filter(Code != '50') %>%  # exclude generic data code
-  group_by(TEAGE, Description, work.status) %>%
+work.gif <- weigh_it(df = sum.2018, groups = c('TEAGE', 'work.status')) %>% 
+  # match based on regex code in curated.codes
+  fuzzyjoin::regex_left_join(x = ., y = curated.codes, by = c(activity = 'activity')) %>%
+  filter(activity.y != '50') %>%  # exclude generic data code
+  group_by(TEAGE, description, work.status) %>%
   summarize(weighted.minutes = sum(weighted.minutes)) %>% 
   mutate(work.status = if_else(work.status, 'An average work day', 'An average leisure day')) %>% 
   ggplot(aes(x = TEAGE, y = weighted.minutes)) +
-  geom_point(alpha = 0.1) +
+  geom_point(alpha = 0.1, color = blog.color) +
   geom_smooth(method = lm, formula = y ~ splines::bs(x, 4),
               se = FALSE, 
               color = blog.color,
               linetype = 'dashed') +
   scale_y_continuous(label = function(x) sprintf("%2d:%02d", as.integer(x %/% 60), as.integer(x %% 60))) +
-  facet_wrap(~Description, scales = 'free_y', ncol = 3) +
+  facet_wrap(~description, scales = 'free_y', ncol = 3) +
   labs(title = "{closest_state}",
        subtitle = "Average daily time spent on activity by age",
        caption = 'Source: 2018 American Time Use Survey\nWork day defined as working two or more hours',
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Analysis of the [American Time Use Survey](https://www.bls.gov/tus/)
 - [x] Summarize of activities by age
 - [x] Summarize of activities by age and working status
 - [ ] Double check specific activities are categorized correctly (e.g. travel should not include commuting)
-- [ ] Pull out sleep from Personal Care
+- [x] Pull out sleep from Personal Care
 - [ ] Explore trends by income
   - What activities are more indicative of income
   - Do higher income people work more or less
@@ -16,7 +16,15 @@ Analysis of the [American Time Use Survey](https://www.bls.gov/tus/)
 <br>
 
 <p align="center">
+<img src="Plots/TV_by_age_sex.svg" width=79%>
+</p>
+
+<p align="center">
 <img src="Plots/Activities_by_age.svg" width=79%>
+</p>
+
+<p align="center">
+<img src="Plots/Activities_by_age_sex.svg" width=79%>
 </p>
 
 <p align="center">
@@ -27,3 +35,6 @@ Analysis of the [American Time Use Survey](https://www.bls.gov/tus/)
 <img src="Plots/work.gif" width=79%>
 </p>
 
+<p align="center">
+<img src="Plots/Activities_by_age_sex_workonly.svg" width=79%>
+</p>
