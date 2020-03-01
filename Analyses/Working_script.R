e2b5c7cc9a21132ff6eb549a8103ba9b6c8f9aca
joemarlo <joe.marlo.09@gmail.com>
2020-02-29 19:59:06 -0500

    added sports by income

--- Analyses/Working_script.R
+++ Analyses/Working_script.R
@@ -101,7 +101,7 @@ atussum_2018 %>%
   scale_color_manual(values = c('darkgreen', 'firebrick3','royalblue3'),
                      labels = c( '$0-50k', '$50-100k', '$100-150k')) +
   facet_wrap(~description, scales = 'free_y', ncol = 2) +
-  labs(title = "Average daily time spent on activity by household income",
+  labs(title = "Average daily time spent on activity by household income during work days",
        subtitle = "2018 American Time Use Survey",
        caption = 'Work day defined as working two or more hours',
        x = "Age",
@@ -118,5 +118,117 @@ ggsave(filename = "Plots/Activities_by_age_income.svg",
 
 
 # games -------------------------------------------------------------------
+
+game.codes <- colnames(atussum_2018)[colnames(atussum_2018) %in% paste0('t', 130101:130199)]
+
+# average minutes per day in activity by income
+atussum_2018 %>% 
+  # join with hh income from CPS
+  left_join(distinct(atuscps_2018[, c('TUCASEID', 'HEFAMINC')])) %>%
+  left_join(income.levels) %>% 
+  mutate(HH.inc.cut = cut(HH.income,
+                          breaks = seq(0, 150000, by = 50000), 
+                          include.lowest = TRUE, ordered_result = TRUE),
+         HH.inc.cut = factor(HH.inc.cut, levels = levels(HH.inc.cut))) %>%
+  apply_weights(df = ., groups = c('HH.inc.cut'),
+                activities = game.codes) %>% 
+  left_join(specific.codes %>% mutate(Code = paste0('t', Code)), 
+            by = c('activity' = 'Code')) %>% 
+  ggplot(aes(x = reorder(Description, -weighted.minutes), y = weighted.minutes, 
+             group = HH.inc.cut, fill = HH.inc.cut)) +
+  geom_col(position = 'stack', color = 'white') +
+  scale_fill_discrete(labels = c('$0-50k', '$50-100k', '$100-150k')) +
+  coord_flip() +
+  labs(title = 'Average daily time spent on activity by household income',
+       x = NULL,
+       y = 'Minutes') +
+  theme(legend.position = 'bottom',
+        legend.title = element_blank())
+
+# participation rate
+atussum_2018 %>% 
+  select(TUFINLWGT, game.codes) %>% 
+  pivot_longer(cols = -TUFINLWGT) %>% 
+  group_by(name) %>% 
+  summarize(part.rate = sum(TUFINLWGT * (value > 0)) / sum(TUFINLWGT)) %>% 
+  left_join(specific.codes %>% mutate(Code = paste0('t', Code)), 
+            by = c('name' = 'Code')) %>% 
+  ggplot(aes(x = reorder(Description, -part.rate), y = part.rate)) +
+  geom_col(position = 'stack', color = 'white') +
+  # scale_fill_discrete(labels = c('$0-50k', '$50-100k', '$100-150k')) +
+  scale_y_continuous(labels = scales::percent_format()) +
+  coord_flip() +
+  labs(title = 'Average daily time spent on activity by household income',
+       x = NULL,
+       y = 'Participation rate') +
+  theme(legend.position = 'bottom',
+        legend.title = element_blank())
+
+# participation rates in activity by income, each one scaled to sum to 1
+atussum_2018 %>% 
+  select(TUFINLWGT, TUCASEID, game.codes) %>% 
+  left_join(distinct(atuscps_2018[, c('TUCASEID', 'HEFAMINC')])) %>%
+  # re bucket incomes into five
+  mutate(HEFAMINC = case_when(
+    HEFAMINC %in% 1:7 ~ 1,
+    HEFAMINC %in% 8:11 ~ 8,
+    HEFAMINC %in% 12:13 ~ 12,
+    HEFAMINC %in% 14:15 ~ 14,
+    HEFAMINC == 16 ~ 16)) %>% 
+  left_join(income.levels) %>% 
+  select(-TUCASEID, -HEFAMINC) %>% 
+  pivot_longer(cols = -c('TUFINLWGT', 'HH.income')) %>% 
+  group_by(name, HH.income) %>% 
+  summarize(part.rate = sum(TUFINLWGT * (value > 0)) / sum(TUFINLWGT)) %>% 
+  # filter out low partiicpation rate sports
+  group_by(name) %>% 
+  mutate(total.part.rate = sum(part.rate)) %>% 
+  ungroup() %>% 
+  filter(total.part.rate >= quantile(total.part.rate, .30)) %>% 
+  left_join(specific.codes %>% mutate(Code = paste0('t', Code)), 
+            by = c('name' = 'Code')) %>% 
+  filter(!grepl('n\\.e\\.c', Description)) %>%
+  group_by(name) %>% 
+  mutate(part.rate = part.rate / sum(part.rate),
+         # calculate income weighted by part.rate for sorting purposes
+         mean.inc = mean(HH.income * part.rate)) %>%
+  ungroup() %>%
+  ggplot(aes(x = reorder(Description, -mean.inc), y = part.rate, 
+             group = rev(HH.income), fill = as.factor(HH.income))) +
+  geom_col(position = 'stack', color = 'white') +
+  scale_y_continuous(labels = NULL) +
+  scale_fill_manual(values = scales::seq_gradient_pal(low = '#91ebc0', high = '#1d4f37')
+                    (seq(0,1, length.out = 5)),
+                    labels = c('$0-25k', '$25-50k', '$50-75k', '$75-150k', '$150k+')) +
+  coord_flip() +
+  labs(title = 'Sports and recreation by income distribution',
+       x = NULL,
+       y = 'Proportion of participation',
+       caption = '2018 American Time Use Survey') +
+  theme(legend.position = 'bottom',
+        legend.title = element_blank(),
+        plot.caption = element_text(face = "italic",
+                                    size = 5,
+                                    color = 'grey50'))
+
+ggsave(filename = "Plots/Activities_by_income.svg",
+       plot = last_plot(),
+       device = "svg",
+       width = 9,
+       height = 7)
+
+# eating out --------------------------------------------------------------
+
+specific.codes %>%
+  filter(grepl('out', Description, ignore.case = TRUE))
+
+
+
+
+# waiting -----------------------------------------------------------------
+
+specific.codes %>%
+  filter(grepl('wait', Description, ignore.case = TRUE)) %>% View
+
 
 
--- README.md
+++ README.md
@@ -9,48 +9,28 @@ Analysis of the [American Time Use Survey](https://www.bls.gov/tus/)
 - [x] Summarize of activities by age and working status
 - [ ] Double check specific activities are categorized correctly (e.g. travel should not include commuting)
 - [x] Pull out sleep from Personal Care
+- [ ] Integrate multi-year data
 - [ ] Explore trends by income
   - What activities are more indicative of income
   - Do higher income people work more or less
 - [ ] Explore ... over the multi years
   - Waiting associated with working / Waiting associated with work-related activities
   - Work, other job(s)
-  - Security related to attending sporting events
-- [ ] Explore sports by wealth
+  - Security related to attending sporting events / Security procedures rel. to arts & entertainment
+  - Eating out by income
+  - Waiting
+  - Houseplants 020501
+- [ ] Rank sports by income
   - 130101:130199
 
 
 <br>
 
+
 <p align="center">
 <img src="Plots/TV_by_age_sex.svg" width=79%>
-</p>
-
-<!--
-<p align="center">
-<img src="Plots/Activities_by_age.svg" width=79%>
-</p>
-
-<p align="center">
-<img src="Plots/Activities_by_age_sex.svg" width=79%>
-</p>
-
-<p align="center">
-<img src="Plots/Activities_by_age_sex_workonly.svg" width=79%>
-</p>
--->
-
-<p align="center">
 <img src="Plots/Activities_by_age_work.svg" width=79%>
-</p>
-
-<!--
-<p align="center">
-<img src="Plots/work.gif" width=79%>
-</p>
--->
-
-<p align="center">
 <img src="Plots/Activities_by_age_income.svg" width=79%>
+<img src="Plots/Activities_by_income.svg" width=79%>
 </p>
 
